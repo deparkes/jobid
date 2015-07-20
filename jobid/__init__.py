0b7aee0cc78ece9d41d4659853cad46d7f2f9f08
Duncan Parkes <ppxdep@nottingham.ac.uk>
2015-07-20 17:29:06 +0100

    Fix problems

--- jobid/__init__.py
+++ jobid/__init__.py
@@ -24,18 +24,32 @@ import csv
 import os
 import socket
 from datetime import datetime
+from os.path import expanduser
 
 def main(argv):
     """
     Run the jobid function
     """
-    jobid(argv)
+    try:
+        comment = argv[1]
+    except:
+        comment = "-"
+    
+    try:
+        outdir = argv[2]
+    except:
+        outdir = expanduser("~")
+    
+    jobid(outdir, comment)
 
-def jobid(argv):
+def jobid(outdir=expanduser("~"), comment="-"):
     """
     Create a jobid number for keeping track of simulation output etc.
     """
-    jobid_log = os.path.dirname(os.path.realpath(sys.argv[0])) + "\jobid.log"
+
+    print outdir
+    print comment
+    jobid_log = outdir + "/jobid.log"
     hostname = socket.gethostname()
     time_stamp = datetime.now().strftime("%Y-%m-%d %H:%M")
     # Open file in binary mode for reading and writing
@@ -43,14 +57,14 @@ def jobid(argv):
     # in loads of empty lines.
     # Read the lines into an list, rows
     with open(jobid_log, "ab+") as out_file:
-        reader = csv.reader(f)
+        reader = csv.reader(out_file)
         rows = list(reader)
 
     # If the file is empty, then we have just created it
     # In which case add a line for column headings
     if os.stat(jobid_log).st_size == 0:
         with open(jobid_log, "ab+") as out_file:
-            writer = csv.writer(f)
+            writer = csv.writer(out_file)
             writer.writerow(["JOBID", "time", "comment"])
 
     # Increment to get next JOBID
@@ -62,10 +76,7 @@ def jobid(argv):
         job_id = 0
 
     # Look for a comment provided as an argument
-    try:
-        comment = sys.argv[1]
-    except:
-        comment = "-"
+
     # Including the hostname in the JOBID makes it easier
     # to find files later e.g. search for '1234.hostname',
     # rather than '1234', which there may well be many of
